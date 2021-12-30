737907c3635c4f3ca9a82c330b3284ed45dace5e
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-30 16:29:51 +0900

    style. 다크 테마 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { AppBar, Container, MenuItem, Select, Toolbar, Typography } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core'
+import { AppBar, Container, MenuItem, Select, Toolbar, Typography, ThemeProvider } from '@material-ui/core'
+import { makeStyles, createTheme } from '@material-ui/core'
 import { useHistory } from 'react-router-dom'
 
 const useStyles = makeStyles(() => ({
@@ -17,25 +17,35 @@ const Header = () => {
 
   const classes = useStyles();
   const history = useHistory();
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#fff',
+      },
+      type: 'dark'
+    }
+  })
 
   return (
-    <AppBar color='transparent' postion='static'>
-      <Container>
-        <Toolbar>
-          <Typography 
-            onClick={() => history.push('/')} 
-            className={classes.title}
-          >Crypto App</Typography>
-          <Select 
-            variant="outlined"
-            style={{ width: 100, height: 40, marginLeft: 15 }}
-          >
-            <MenuItem value={'USD'}>USD</MenuItem>
-            <MenuItem value={'KRW'}>KRW</MenuItem>
-          </Select>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color='transparent' postion='static'>
+        <Container>
+          <Toolbar>
+            <Typography 
+              onClick={() => history.push('/')} 
+              className={classes.title}
+            >Crypto App</Typography>
+            <Select 
+              variant="outlined"
+              style={{ width: 100, height: 40, marginRight: 15 }}
+            >
+              <MenuItem value={'USD'}>USD</MenuItem>
+              <MenuItem value={'KRW'}>KRW</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   )
 }
 
