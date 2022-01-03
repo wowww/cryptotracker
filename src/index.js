b77d77ea4f36e31e1802e425f0166f31da476448
Eunjee <58850132+wowww@users.noreply.github.com>
2022-01-03 11:21:47 +0900

    style. carousel 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import CryptoContext from './CryptoContext'
 import './index.css';
 import App from './App';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 ReactDOM.render(
   <React.StrictMode>
