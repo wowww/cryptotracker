5387e89b5f1e128e3c2438bedd548a6a810b0cc4
Eunjee <58850132+wowww@users.noreply.github.com>
2022-01-03 13:51:00 +0900

    feat. coin 가격이 포함된 배열 가져와서 Table Head 작성

--- src/components/CoinTable.js
+++ src/components/CoinTable.js
@@ -6,8 +6,17 @@ import { CryptoState } from '../CryptoContext'
 import {
   Container,
   createTheme,
+  TableCell,
+  LinearProgress,
+  ThemeProvider,
   Typography,
   TextField,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableContainer,
+  Table,
+  // Paper,
 } from "@material-ui/core";
 
 const CoinTable = () => {
@@ -55,6 +64,33 @@ const CoinTable = () => {
           style={{ marginBottom: 20, width: "100%" }}
           onChange={(e) => setSearch(e.target.value)}
         />
+        <TableContainer style={{ backgroundColor: "gold" }}>
+          {
+            loading ? (
+              <LinearProgress></LinearProgress>
+            ) : (
+              <Table>
+                <TableHead style={{ backgroundColor: "#EEBC1D" }}>
+                  <TableRow>
+                    {["Coin", "Price", "24h Change", "Market Cap"].map((head) => (
+                      <TableCell
+                        style={{
+                          color: "black",
+                          fontWeight: "700",
+                          fontFamily: "Montserrat",
+                        }}
+                        key={head}
+                        align={head === "Coin" ? "" : "right"}
+                      >
+                        {head}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+              </Table>
+            )
+          }
+        </TableContainer>
       </Container>
     </ThemeProvider>
   )
