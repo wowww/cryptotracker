ac6fa78ff816b52bf51a83cca104c7bcf1421e70
Eunjee <58850132+wowww@users.noreply.github.com>
2022-01-03 17:01:12 +0900

    docs. Coin page

--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import { CryptoState } from '../CryptoContext'
+import { SingleCoin } from '../config/api'
 
 const CoinPage = () => {
+  const { id } = useParams()
+  const [coin, setCoin] = useState()
+
+  const { currency, symbol } = CryptoState();
+
+  const fetchCoins = async() => {
+    const { data } = await axios.get(SingleCoin(id))
+
+    setCoin(data)
+  }
+
+  useEffect(() => {
+    fetchCoins()
+
+  },[])
+
   return (
     <div>
       
