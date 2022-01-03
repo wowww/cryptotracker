0e76dd601be982b08a565d4ce89d8b5ae8610725
Eunjee <58850132+wowww@users.noreply.github.com>
2022-01-03 13:16:35 +0900

    docs. import CoinTable

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/banner/Banner'
+import CoinTable from '../components/CoinTable'
 
 const Homepage = () => {
   return (
     <>
       <Banner />
+      <CoinTable />
     </>
   )
 }
