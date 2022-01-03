bf560f6cd6bf5c5b736acf18ccaae1fa26b7d4df
Eunjee <58850132+wowww@users.noreply.github.com>
2022-01-03 11:26:35 +0900

    feat. CryptoState가져와서 Carousel에 데이터 넣어 화면에 출력

--- src/components/banner/Carousel.js
+++ src/components/banner/Carousel.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core'
-
+import axios from 'axios'
+import { Link } from "react-router-dom";
+import { CryptoState } from '../../CryptoContext'
+import { TrendingCoins } from '../../config/api'
+import AliceCarousel from 'react-alice-carousel';
 
 const useStyles = makeStyles(() => ({
   carousel: {
@@ -11,11 +15,65 @@ const useStyles = makeStyles(() => ({
 }))
 
 const Carousel = () => {
+  const [ trending, setTrending ] = useState([])
   const classes = useStyles()
+  const { currency } = CryptoState();
+
+  const fetchTrendingCoins = async () => {
+    const { data } = await axios.get(TrendingCoins(currency))
+    
+    setTrending(data)
+  }
+
+  console.log(trending)
+
+  useEffect(() => {
+    fetchTrendingCoins();
+  }, [currency])
+
+  const items = trending.map((coin) => {
+    let profit = coin.price_change_percentage_24h >= 0;
+    return (
+      <Link
+        className={classes.carouselItem}
+        to={`/coin/${coin.id}`}
+      >
+        <img 
+          src={coin?.image}
+          alt={coin.name}
+          height="80"
+          style={{marginBottom: 10}}
+        />
+        <span>
+          {coin?.symbol} &nbsp;
+          <span>{profit && "+"} {coin?.price_change_percentage_24h?.toFixed(2)}%</span>
+        </span>
+      </Link>
+    )
+  })
+
+  const responsive = {
+    0: {
+      items: 2,
+    },
+    512: {
+      items: 4,
+    },
+  };
 
   return (
     <div className={classes.carousel}>
-      
+      <AliceCarousel
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        disableButtonsControls
+        responsive={responsive}
+        items={items}
+        autoPlay
+      />
     </div>
   )
 }
