0fdbfac1a67ddb7698ad1a0f4bc9b95a0c899bf1
Eunjee <58850132+wowww@users.noreply.github.com>
2022-01-03 10:01:46 +0900

    style. 배너 스타일링

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import { Container, makeStyles, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles(() => ({
+  banner: {
+    backgroundImage: "url(./banner2.jpg)",
+  },
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+}))
+
 
 const Banner = () => {
+  const classes = useStyles()
   return (
-    <>
-      Banner
-    </>
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography 
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat"
+            }}
+          >
+            Crypto 
+          </Typography>
+        </div>
+      </Container>
+    </div>
   )
 }
 
