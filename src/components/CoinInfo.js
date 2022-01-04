0ede29a9d042f21f44d92a1695fcb17973546a79
Eunjee <58850132+wowww@users.noreply.github.com>
2022-01-04 10:23:08 +0900

    docs. CoinInfo

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,6 +1,48 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { HistoricalChart } from "../config/api";
+import { Line } from "react-chartjs-2";
+import {
+  CircularProgress,
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
+// import SelectButton from "./SelectButton";
+import { chartDays } from "../config/data";
+import { CryptoState } from "../CryptoContext";
 
-const CoinInfo = () => {
+const CoinInfo = ({ coin }) => {
+  const [ historicData, setHistoricData ] = useState()
+  const [ days, setDays ] = useState(1)
+
+  const { currency } = CryptoState();
+
+  const useStyles = makeStyles((theme) => ({
+
+  }))
+
+  const classes = useStyles();
+
+  const fetchHistoricData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+    // setflag(true);
+    setHistoricData(data.prices);
+  };
+
+  useEffect(() => {
+    fetchHistoricData();
+  }, [days]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
   return (
     <div>
       
